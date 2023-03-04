82e437ef37d490cf7ee02e143d5f98ce245bf610
Samuel Ziegler (Local) <sumpfsamuel@gmail.com>
2023-03-04 17:03:34 +0100

    documentation

--- app/src/main/java/com/example/se_einzelarbeit_ziegler/Util/DialogUtil.java
+++ app/src/main/java/com/example/se_einzelarbeit_ziegler/Util/DialogUtil.java
@@ -1,17 +1,28 @@
+// Samuel Ziegler 2023
 package com.example.se_einzelarbeit_ziegler.Util;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import com.example.se_einzelarbeit_ziegler.MainActivity;
 
+/**
+ * Utils for Dialog based things
+ */
 public class DialogUtil {
-    static public AlertDialog createDialog(String title, String msg, MainActivity context){
+    /**
+     * Create a dialog with the given parameters
+     * @param title dialog title
+     * @param msg dialog output message
+     * @param context acivity
+     * @return the finished dialog
+     */
+    static public AlertDialog createDialog(String title, String msg, MainActivity context) {
         AlertDialog dialog = new AlertDialog.Builder(context)
                 .setTitle(title)
                 .setMessage(msg)
                 .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        //für später eventuell
+                        //mandatory method, but useless
                     }
                 }).create();
 
