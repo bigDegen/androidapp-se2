d99a9fa16e0d770295a086e9e5e68995fb05f0e2
Samuel Ziegler (Local) <sumpfsamuel@gmail.com>
2023-03-04 17:04:47 +0100

    Revert "Revert "documentation""
    This reverts commit 1398522ae4dc32eb9c25bdcec95a83255104a1b3.

--- app/src/main/java/com/example/se_einzelarbeit_ziegler/Util/ActivityUtil.java
+++ app/src/main/java/com/example/se_einzelarbeit_ziegler/Util/ActivityUtil.java
@@ -1,11 +1,21 @@
+// Samuel Ziegler 2023
 package com.example.se_einzelarbeit_ziegler.Util;
 
 import android.view.View;
 import com.example.se_einzelarbeit_ziegler.MainActivity;
 
+/**
+ * Utils for the Acivity
+ */
 public class ActivityUtil {
 
-    public static View getView(MainActivity context, int id){
+    /**
+     * Gets the view by the R.id
+     * @param context activity
+     * @param id the R.id
+     * @return the view of the id
+     */
+    public static View getView(MainActivity context, int id) {
         return context.findViewById(id);
     }
 }
