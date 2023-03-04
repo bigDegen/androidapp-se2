9fa809dd6673602a4468eacb635caa3860e90b0b
Samuel Ziegler (Local) <sumpfsamuel@gmail.com>
2023-03-04 17:04:01 +0100

    added gcd function + documentation

--- app/src/main/java/com/example/se_einzelarbeit_ziegler/Util/MathUtil.java
+++ app/src/main/java/com/example/se_einzelarbeit_ziegler/Util/MathUtil.java
@@ -1,8 +1,21 @@
+// Samuel Ziegler 2023
 package com.example.se_einzelarbeit_ziegler.Util;
 
+/**
+ * Utils for mathematical calculations
+ */
 public class MathUtil {
 
-    static public int getGcc(){
-        return 1;
+    /**
+     * Calculate the greatest common divider of two numbers
+     * @param firstNumber first number
+     * @param secondNumber second number
+     * @return the gcd
+     */
+    static public int getGcd(int firstNumber, int secondNumber) {
+        int divisor = 1;
+        for (int i = 1; i <= firstNumber && i <= secondNumber; i++)
+            if (firstNumber% i == 0 && secondNumber % i == 0) divisor = i;
+        return divisor;
     }
 }
