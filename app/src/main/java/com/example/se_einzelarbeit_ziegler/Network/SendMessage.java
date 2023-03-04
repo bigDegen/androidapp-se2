96d96f00881484140e5d2cc13dc2b1f190d4dacf
Samuel Ziegler (Local) <sumpfsamuel@gmail.com>
2023-03-04 17:14:22 +0100

    reformatting + button handlers + documented

--- app/src/main/java/com/example/se_einzelarbeit_ziegler/Network/SendMessage.java
+++ app/src/main/java/com/example/se_einzelarbeit_ziegler/Network/SendMessage.java
@@ -1,6 +1,6 @@
+// Samuel Ziegler 2023
 package com.example.se_einzelarbeit_ziegler.Network;
 
-import android.widget.EditText;
 import android.widget.TextView;
 import com.example.se_einzelarbeit_ziegler.MainActivity;
 import com.example.se_einzelarbeit_ziegler.R;
@@ -11,44 +11,70 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
+/**
+ * Class for message sending and retrieving via TCP Socket
+ */
 public class SendMessage implements Runnable {
-    private MainActivity context;
-    private boolean correctInput;
+    private final MainActivity context;
+    private final boolean correctInput;
+    private final String matno;
 
-    public SendMessage(MainActivity context, boolean correctInput) {
+    /**
+     * Constructor for SendMessage Class
+     *
+     * @param context      acivity
+     * @param correctInput boolean for correct number input
+     * @param matno        the matrikelnumber
+     */
+    public SendMessage(MainActivity context, boolean correctInput, String matno) {
         this.context = context;
         this.correctInput = correctInput;
+        this.matno = matno;
     }
 
+    /**
+     * Standard runnable run function
+     * This function handles all of the magic,
+     * connection -> writing to server -> reading from server(as string) -> closing connection
+     */
     @Override
     public void run() {
-        EditText inputMatNo = (EditText) ActivityUtil.getView(context, R.id.matNo);
         TextView outputTextView = (TextView) ActivityUtil.getView(context, R.id.txtOutput);
 
-        String matNo = inputMatNo.getText().toString();
-
         if (correctInput) {
+            outputTextView.setText("initializing connection...");
             Socket clientSocket = new Socket();
             try {
-                clientSocket = new Socket(context.HOST, context.PORT);
+                clientSocket = new Socket(MainActivity.HOST, MainActivity.PORT);
 
                 DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
                 BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-                output.writeBytes(matNo + "\n");
+                outputTextView.setText("fetching please be patient...");
+
+                output.writeBytes(this.matno + "\n");
 
                 outputTextView.setText(input.readLine());
                 input.close();
                 output.close();
                 clientSocket.close();
             } catch (Exception ex) {
-                delegateErrorDialog(ex.getMessage().toString());
-                System.out.println(ex.toString());
+                delegateErrorDialog(ex.getMessage());
+                System.out.println(ex);
             }
-        }
+        } else outputTextView.setText("Matrikelnumber was too short, please try again!");
     }
 
+    /**
+     * Delegate the show error dialog to the main ui thread to show the user his wrongdoing
+     *
+     * @param errorMsg the exception message
+     */
     private void delegateErrorDialog(String errorMsg) {
-        context.displayError(errorMsg);
+        //run on main thread, otherwise the program makes boom boom
+        context.runOnUiThread(() -> {
+            context.displayError(errorMsg);
+        });
+
     }
 }
