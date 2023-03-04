8cf0f2956fc254d126816ff7bdbf45670ff681be
Samuel Ziegler (Local) <sumpfsamuel@gmail.com>
2023-03-04 17:04:11 +0100

    documentation

--- app/src/main/java/com/example/se_einzelarbeit_ziegler/MainActivity.java
+++ app/src/main/java/com/example/se_einzelarbeit_ziegler/MainActivity.java
@@ -1,57 +1,130 @@
 // Samuel Ziegler 2023
 package com.example.se_einzelarbeit_ziegler;
 
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import com.example.se_einzelarbeit_ziegler.Network.SendMessage;
 import com.example.se_einzelarbeit_ziegler.Util.ActivityUtil;
 import com.example.se_einzelarbeit_ziegler.Util.DialogUtil;
+import com.example.se_einzelarbeit_ziegler.Util.MathUtil;
 
-
+/**
+ * This class servers as the code behind the main_act View
+ */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    //Server Information
+    //Server Information host+port
     public final static String HOST = "se2-isys.aau.at";
     public final static int PORT = 53212;
     //Output Text field
     TextView output;
     //GET Information Button
-    Button btnFetchNumber;
+    Button btnFetchNumber, btnCalcDivisor;
+    //Input field
+    EditText inputMatNo;
 
+    /**
+     * Initialization of the View and its components
+     * @param savedInstanceState Instance State
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_act);
 
         //Initalize OutputField
-        output = (TextView) ActivityUtil.getView(this,R.id.txtOutput);
+        output = (TextView) ActivityUtil.getView(this, R.id.txtOutput);
 
-        //Intialization Button
+        //Intialization Buttons
         btnFetchNumber = (Button) ActivityUtil.getView(this, R.id.btnFetchNumber);
         btnFetchNumber.setOnClickListener(MainActivity.this);
 
+        btnCalcDivisor = (Button) ActivityUtil.getView(this, R.id.btnCalcDivisor);
+        btnCalcDivisor.setOnClickListener(MainActivity.this);
+
+        //Initialize Matrikelnumber input field
+        inputMatNo = (EditText) ActivityUtil.getView(this, R.id.matNo);
+
     }
 
+    /**
+     * This function serves as the Event handler for Button clicks
+     * @param v the corrosponding view
+     */
     @Override
     public void onClick(View v) {
-        EditText inputMatNo = (EditText) ActivityUtil.getView(this, R.id.matNo);
+        switch (v.getId())
+        {
+            case R.id.btnFetchNumber:
+                onBTNgetinformationClick();
+                break;
+            case R.id.btnCalcDivisor:
+                onBTNcalcdivisorClick();
+                break;
+            default:
+                break;
 
-        boolean correctInput = true;
-        if (inputMatNo.getText().toString().length()<7) {
-            DialogUtil.createDialog("Wrong input", "Thy input should be atleast 7 digits!", this).show();
-            correctInput=false;
         }
-
-        SendMessage messageSender = new SendMessage(this, correctInput);
-
-        // Sending message to Server, HOST and PORT are defined above
-        new Thread(messageSender).start();
     }
 
-    public void displayError(String errorMsg){
+    /**
+     * ***************************************************************************************************
+     * This function gets invoked when the Get common divisor button is pressed
+     * It goes throw the matrikelnumber and tries to calculate a common divisor,
+     * two numbers get passed into the MathUtil.class - getGcd funtion
+     * ***************************************************************************************************
+     * (i could not extract from the assignement if the order is of relevance so i did not start the second
+     * for loop at position i but rather skipped the same index numbers)
+     */
+    private void onBTNcalcdivisorClick() {
+        output.setText("calculating common divisor...");
+        int firstnumber=0, secondnumber=0, divisor=1;
+        char[] matrikelnumberCharArr = inputMatNo.getText().toString().toCharArray();
+        //go through the matrikelnumber and find a common divisor by calculating gcd
+        for (int i =0; i<matrikelnumberCharArr.length; i++){
+            if (divisor>1)break;
+            firstnumber = Character.getNumericValue(matrikelnumberCharArr[i]);
+            for (int k = 0; k<matrikelnumberCharArr.length;k++){
+                if (divisor>1)break;
+                if (i!=k) {
+                    secondnumber = Character.getNumericValue(matrikelnumberCharArr[k]);
+                    divisor = MathUtil.getGcd(firstnumber, secondnumber);
+                }
+            }
+        }
+        output.setText("The number "+firstnumber+" and "+secondnumber+" in the Matrikelnumber "+inputMatNo.getText().toString()+", can be divided by "+divisor+"!");
+    }
+
+    /**
+     * This function gets invoked when the Get Information button is pressed
+     * It validates the Matrikelnumber and sends it to the SendMessage class which handles the TCP
+     * connection aswell as the sending of the message
+     */
+    private void onBTNgetinformationClick() {
+        boolean correctInput = true;
+        if (inputMatNo.getText().toString().length() < 7) {
+            correctInput = false;
+        }
+
+        SendMessage messageSender = new SendMessage(this, correctInput, inputMatNo.getText().toString());
+
+        // Sending message to Server, HOST and PORT are defined above
+        try {
+            new Thread(messageSender).start();
+        } catch (Exception ex) {
+            displayError(ex.getMessage());
+        }
+    }
+
+    /**
+     * Function for showing dialog fields for error messages
+     * The error message gets passed in the DialogUtil class
+     * @param errorMsg an error message from an exception
+     */
+    public void displayError(String errorMsg) {
         DialogUtil.createDialog("Error while fetching", errorMsg, this).show();
     }
 }
--- app/src/main/java/com/example/se_einzelarbeit_ziegler/Network/SendMessage.java
+++ app/src/main/java/com/example/se_einzelarbeit_ziegler/Network/SendMessage.java
@@ -1,54 +1,78 @@
+// Samuel Ziegler 2023
 package com.example.se_einzelarbeit_ziegler.Network;
 
-import android.widget.EditText;
 import android.widget.TextView;
 import com.example.se_einzelarbeit_ziegler.MainActivity;
 import com.example.se_einzelarbeit_ziegler.R;
 import com.example.se_einzelarbeit_ziegler.Util.ActivityUtil;
-import com.example.se_einzelarbeit_ziegler.Util.DialogUtil;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
+/**
+ * Class for message sending and retrieving via TCP Socket
+ */
 public class SendMessage implements Runnable {
-    private MainActivity context;
-    private boolean correctInput;
+    private final MainActivity context;
+    private final boolean correctInput;
+    private final String matno;
 
-    public SendMessage(MainActivity context, boolean correctInput){
+    /**
+     * Constructor for SendMessage Class
+     * @param context acivity
+     * @param correctInput boolean for correct number input
+     * @param matno the matrikelnumber
+     */
+    public SendMessage(MainActivity context, boolean correctInput, String matno) {
         this.context = context;
         this.correctInput = correctInput;
+        this.matno = matno;
     }
 
+    /**
+     * Standard runnable run function
+     * This function handles all of the magic,
+     * connection -> writing to server -> reading from server(as string) -> closing connection
+     */
     @Override
     public void run() {
-        EditText inputMatNo = (EditText) ActivityUtil.getView(context, R.id.matNo);
         TextView outputTextView = (TextView) ActivityUtil.getView(context, R.id.txtOutput);
 
-        String matNo = inputMatNo.getText().toString();
+        if (correctInput) {
+            outputTextView.setText("initializing connection...");
+            Socket clientSocket = new Socket();
+            try {
+                clientSocket = new Socket(MainActivity.HOST, MainActivity.PORT);
 
-        if (correctInput){
-                Socket clientSocket = new Socket();
-                try {
-                    clientSocket = new Socket(context.HOST,context.PORT);
+                DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
+                BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-                    DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
-                    BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                outputTextView.setText("fetching please be patient...");
 
-                    output.writeBytes(matNo+"\n");
+                output.writeBytes(this.matno + "\n");
 
-                    outputTextView.setText(input.readLine());
-                    input.close();
-                    output.close();
-                    clientSocket.close();
-                }catch (Exception ex){
-                    delegateErrorDialog(ex.getMessage().toString());
-                    System.out.println(ex.toString());
-                }
-    }}
+                outputTextView.setText(input.readLine());
+                input.close();
+                output.close();
+                clientSocket.close();
+            } catch (Exception ex) {
+                delegateErrorDialog(ex.getMessage());
+                System.out.println(ex);
+            }
+        }else outputTextView.setText("Matrikelnumber was too short, please try again!");
+    }
 
-    private void delegateErrorDialog(String errorMsg){
-        context.displayError(errorMsg);
+    /**
+     * Delegate the show error dialog to the main ui thread to show the user his wrongdoing
+     * @param errorMsg the exception message
+     */
+    private void delegateErrorDialog(String errorMsg) {
+        //run on main thread, otherwise the program makes boom boom
+        context.runOnUiThread(()->{
+            context.displayError(errorMsg);
+        });
+
     }
 }
