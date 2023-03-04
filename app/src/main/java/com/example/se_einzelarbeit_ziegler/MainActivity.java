a0ed13d9620e64fd062490e5d4fc8047b4c93ef9
Samuel Ziegler (Local) <sumpfsamuel@gmail.com>
2023-03-04 15:48:42 +0100

    refactoring

--- app/src/main/java/com/example/se_einzelarbeit_ziegler/MainActivity.java
+++ app/src/main/java/com/example/se_einzelarbeit_ziegler/MainActivity.java
@@ -1,54 +1,57 @@
+// Samuel Ziegler 2023
 package com.example.se_einzelarbeit_ziegler;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.se_einzelarbeit_ziegler.Network.SendMessage;
+import com.example.se_einzelarbeit_ziegler.Util.ActivityUtil;
+import com.example.se_einzelarbeit_ziegler.Util.DialogUtil;
 
-import java.io.Console;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //Server Information
+    public final static String HOST = "se2-isys.aau.at";
+    public final static int PORT = 53212;
+    //Output Text field
     TextView output;
+    //GET Information Button
     Button btnFetchNumber;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_act);
 
         //Initalize OutputField
-        output = (TextView) findViewById(R.id.txtOutput);
+        output = (TextView) ActivityUtil.getView(this,R.id.txtOutput);
 
         //Intialization Button
-        btnFetchNumber = (Button) findViewById(R.id.btnFetchNumber);
-
+        btnFetchNumber = (Button) ActivityUtil.getView(this, R.id.btnFetchNumber);
         btnFetchNumber.setOnClickListener(MainActivity.this);
+
     }
 
     @Override
     public void onClick(View v) {
-        EditText inputMatNo = (EditText) findViewById(R.id.matNo);
-        try {
-            int matNo = Integer.parseInt(inputMatNo.getText().toString());
-        }catch (Exception e){
-            new AlertDialog.Builder(this)
-                    .setTitle("Wrong Input")
-                    .setMessage("Dear Sir, thy input is not a Number!")
-                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            inputMatNo.setText("Wrong input");
-                        }
-                    })
+        EditText inputMatNo = (EditText) ActivityUtil.getView(this, R.id.matNo);
 
-                    // A null listener allows the button to dismiss the dialog and take no further action.
-                    .setNegativeButton(android.R.string.no, null)
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .show();
+        boolean correctInput = true;
+        if (inputMatNo.getText().toString().length()<7) {
+            DialogUtil.createDialog("Wrong input", "Thy input should be atleast 7 digits!", this).show();
+            correctInput=false;
         }
 
+        SendMessage messageSender = new SendMessage(this, correctInput);
 
+        // Sending message to Server, HOST and PORT are defined above
+        new Thread(messageSender).start();
+    }
+
+    public void displayError(String errorMsg){
+        DialogUtil.createDialog("Error while fetching", errorMsg, this).show();
     }
 }
